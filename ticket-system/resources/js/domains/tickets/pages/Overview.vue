b042e862bc6ab0754a6a7dc3ec0affd7bf4342f6
OurenOosterveen <ouren.oosterveen@outlook.com>
2023-07-27 11:20:04 +0200

    basic overview

--- ticket-system/resources/js/domains/tickets/pages/Overview.vue
+++ ticket-system/resources/js/domains/tickets/pages/Overview.vue
@@ -1,16 +1,25 @@
 <template>
-    <div v-for="ticket in tickets" :key="ticket.id">
-        {{ ticket.title }}
+    <div v-for="ticket in tickets" :key="ticket.id" class="ticket-card">
+        <h2> {{ ticket.title }} </h2>
+        <p> {{ ticket.content }} </p>
     </div>
 </template>
 <script setup lang="ts">
 import {onMounted} from 'vue';
 import {ticketStore} from '..';
 
-const tickets = ticketStore.getters.all; 
+const tickets =  ticketStore.getters.all; 
 
 onMounted(async () => {
     await ticketStore.actions.getAll();
-    console.log(ticketStore.getters.all)
 })
 </script>
+<style>
+    .ticket-card {
+        padding: .5rem;
+        background-color:lightskyblue;
+        border-radius: 15px;
+        margin-top: 2rem;
+        width: 100%;
+    }
+</style>
