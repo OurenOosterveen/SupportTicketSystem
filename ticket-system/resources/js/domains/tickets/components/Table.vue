b7f27ddfa3669da9ef438f80627f532b258e6105
OurenOosterveen <ouren.oosterveen@outlook.com>
2023-07-27 11:57:15 +0200

    Moved table to own component

---
+++ ticket-system/resources/js/domains/tickets/components/Table.vue
@@ -0,0 +1,42 @@
+<template>
+    <table class="ticket-table">
+        <tr>
+            <th>Ticket ID</th>
+            <th>Titel</th>
+            <th>Categorie</th>
+            <th>Status</th>
+            <th>Aangemaakt door</th>
+            <th>Aangemaakt op</th>
+            <th>Laatste update</th>
+            <th>Toegewezen aan</th>
+        </tr>
+        <tr v-for="ticket in tickets" :key="ticket.id">
+            <td>{{ ticket.id }}</td>
+            <td>{{ ticket.title }}</td>
+            <td>{{ ticket.category_id }}</td>
+            <td>{{ ticket.status_id }}</td>
+            <td>{{ ticket.user_id }}</td>
+            <td>{{ ticket.created_at }}</td>
+            <td>{{ ticket.updated_at }}</td>
+            <td>{{ ticket.assignee_id }}</td>
+        </tr>
+    </table>
+</template>
+<script setup lang="ts">
+import {Ticket} from '../types';
+
+defineProps<{tickets: Ticket[]}>()
+</script>
+<style>
+    .ticket-table {
+        padding: .5rem;
+        background-color:lightskyblue;
+        margin-top: 2rem;
+        width: 100%;
+        border-collapse: collapse;
+    }
+    td, th {
+        text-align: start;
+        border: 1px solid grey;
+    }
+</style>
--- ticket-system/resources/js/domains/tickets/pages/Overview.vue
+++ ticket-system/resources/js/domains/tickets/pages/Overview.vue
@@ -1,31 +1,11 @@
 <template>
-    <table class="ticket-table">
-        <tr>
-            <th>Ticket ID</th>
-            <th>Titel</th>
-            <th>Categorie</th>
-            <th>Status</th>
-            <th>Aangemaakt door</th>
-            <th>Aangemaakt op</th>
-            <th>Laatste update</th>
-            <th>Toegewezen aan</th>
-        </tr>
-        <tr v-for="ticket in tickets" :key="ticket.id">
-            <td>{{ ticket.id }}</td>
-            <td>{{ ticket.title }}</td>
-            <td>{{ ticket.category_id }}</td>
-            <td>{{ ticket.status_id }}</td>
-            <td>{{ ticket.user_id }}</td>
-            <td>{{ ticket.created_at }}</td>
-            <td>{{ ticket.updated_at }}</td>
-            <td>{{ ticket.assignee_id }}</td>
-        </tr>
-    </table>
+    <Table :tickets="tickets" />
 </template>
 <script setup lang="ts">
 import {computed, onMounted} from 'vue';
 import {getLoggedInUser} from 'domains/auth';
 import {ticketStore} from '..';
+import Table from '../components/Table.vue';
 
 // Show tickets only for current user, all tickets if the current user is an admin
 const tickets = computed(() => getLoggedInUser.value?.is_admin
@@ -37,16 +17,3 @@ onMounted(async () => {
     await ticketStore.actions.getAll();
 })
 </script>
-<style>
-    .ticket-table {
-        padding: .5rem;
-        background-color:lightskyblue;
-        margin-top: 2rem;
-        width: 100%;
-        border-collapse: collapse;
-    }
-    td, th {
-        text-align: start;
-        border: 1px solid grey;
-    }
-</style>
