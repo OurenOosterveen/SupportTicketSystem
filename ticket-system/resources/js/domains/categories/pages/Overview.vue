3de2b4e03d4a289ccbe8cd2446f36fc3d564e3bd
OurenOosterveen <ouren.oosterveen@outlook.com>
2023-07-28 14:59:21 +0200

    Sorting tables

--- ticket-system/resources/js/domains/categories/pages/Overview.vue
+++ ticket-system/resources/js/domains/categories/pages/Overview.vue
@@ -1,11 +1,14 @@
 <template>
-    <CategoryTable :categories="categories" />
+    <CategoryTable v-if="sortedCategories.length" :categories="sortedCategories" />
 </template>
 <script setup lang="ts">
 import {categoryStore} from '..';
+import {computed} from 'vue';
+import {sortBy} from 'services/helpers/sort';
 import CategoryTable from '../components/Table.vue';
 
 categoryStore.actions.getAll();
 const categories = categoryStore.getters.all;
+const sortedCategories = computed(() => sortBy(categories.value, 'title'));
 
 </script>
--- ticket-system/resources/js/domains/tickets/pages/Overview.vue
+++ ticket-system/resources/js/domains/tickets/pages/Overview.vue
@@ -1,9 +1,10 @@
 <template>
-    <TicketTable v-if="tickets.length" :tickets="tickets" />
+    <TicketTable v-if="sortedTickets.length" :tickets="sortedTickets" />
 </template>
 <script setup lang="ts">
 import {computed} from 'vue';
 import {getLoggedInUser} from 'domains/auth';
+import {sortBy} from 'services/helpers/sort';
 import {ticketStore} from '..';
 import TicketTable from '../components/Table.vue';
 
@@ -12,6 +13,8 @@ const tickets = computed(() => getLoggedInUser.value?.is_admin
     ? ticketStore.getters.all.value
     : ticketStore.getters.all.value.filter(ticket => ticket.user_id === getLoggedInUser.value?.id),
 ); 
+
+const sortedTickets = computed(() => sortBy(tickets.value, 'created_at'));
 
 ticketStore.actions.getAll();
 
